ae23496c005035995d8af067b47c1b8bf3f26dc4
jayesh200026 <jayeshkumar8660@gmail.com>
2021-10-06 20:56:57 +0530

    added feature to use instance variable instead of funtional parameter

--- src/com/employeeWage/WageTillCondition.kt
+++ src/com/employeeWage/WageTillCondition.kt
@@ -1,22 +1,22 @@
 package com.employeeWage
 
-class EmployeeWage {
-    companion object {
-        fun computeWage(companyName:String,ratePerHour:Int,maximumHour:Int,maximumDays:Int) {
-            var time = 0
-            var days = 0
-            var totalWage = 0
+class EmployeeWage(val companyName: String, val ratePerHour: Int, val maximumHour: Int, val maximumDays: Int) {
 
-            while (time < maximumHour && days < maximumDays) {
-                val (wage, hours) = DailyWage.computeDailyWage(attendance(),ratePerHour)
-                totalWage += wage
-                time += hours
-                days += 1
-            }
-            println("Company:$companyName")
-            println("Total wage = $totalWage")
-            println("Working Days = $days")
-            println("Working Hours = $time")
+    fun computeWage() {
+        var time = 0
+        var days = 0
+        var totalWage = 0
+
+        while (time < maximumHour && days < maximumDays) {
+            val (wage, hours) = DailyWage.computeDailyWage(attendance(), ratePerHour)
+            totalWage += wage
+            time += hours
+            days += 1
         }
+        println("Company:$companyName")
+        println("Total wage = $totalWage")
+        println("Working Days = $days")
+        println("Working Hours = $time")
     }
+
 }
